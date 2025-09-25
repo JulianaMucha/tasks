e0e97a2889dfb106527ba8364b4deaf035bd0ccd
Megkatjul#22 <jmucha@udel.edu>
2025-09-24 23:32:01 -0400

    First 3 components completed

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import "./App.css";
 import { ChangeType } from "./components/ChangeType";
 import { RevealAnswer } from "./components/RevealAnswer";
-import { StartAttempt } from "./components/StartAttempt";
-import { TwoDice } from "./components/TwoDice";
-import { CycleHoliday } from "./components/CycleHoliday";
+// import { StartAttempt } from "./components/StartAttempt";
+// import { TwoDice } from "./components/TwoDice";
+// import { CycleHoliday } from "./components/CycleHoliday";
 import { Counter } from "./components/Counter";
 
 function App(): React.JSX.Element {
@@ -16,21 +16,21 @@ function App(): React.JSX.Element {
                 <span style={{ color: "red" }}>Juliana Mucha</span>
                 <br></br>
                 <span style={{ font: "verdana" }}>
-                    <h1>Halloween</h1>
+                    <h1>Hi! We are using State!</h1>
                 </span>
             </header>
             <hr></hr>
             <Counter></Counter>
             <hr />
             <RevealAnswer></RevealAnswer>
-            <hr />
+            {/* <hr />
             <StartAttempt></StartAttempt>
             <hr />
-            <TwoDice></TwoDice>
+            <TwoDice></TwoDice> */}
             <hr />
             <ChangeType></ChangeType>
-            <hr />
-            <CycleHoliday></CycleHoliday>
+            {/* <hr />
+            <CycleHoliday></CycleHoliday> */}
         </div>
     );
 }
