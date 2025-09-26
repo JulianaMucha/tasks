29ec8852f25c6dd7ff511a862bee0c21c9f3bb28
Megkatjul#22 <jmucha@udel.edu>
2025-09-25 21:36:42 -0400

    First 5 components completed

--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -12,5 +12,36 @@ export function d6(): number {
 }
 
 export function TwoDice(): React.JSX.Element {
-    return <div>Two Dice</div>;
+    const [leftDieValue, setLeftDieValue] = useState<number>(1);
+    const [rightDieValue, setRightDieValue] = useState<number>(4);
+
+    function rollLeftDie(): void {
+        setLeftDieValue(d6());
+    }
+
+    function rollRightDie(): void {
+        setRightDieValue(d6());
+    }
+
+    function isEqualButNotOnes(): string {
+        return (
+            leftDieValue === rightDieValue ?
+                leftDieValue === 1 ?
+                    "Lose"
+                :   "Win"
+            :   "Neither"
+        );
+    }
+
+    return (
+        <div>
+            left die:
+            <Button onClick={rollLeftDie}>Roll Left</Button>
+            <span data-testid="left-die">{leftDieValue}</span>
+            right die:
+            <Button onClick={rollRightDie}>Roll Right</Button>
+            <span data-testid="right-die">{rightDieValue}</span>
+            <span data-testid="winOrLose?">{isEqualButNotOnes()}</span>
+        </div>
+    );
 }
